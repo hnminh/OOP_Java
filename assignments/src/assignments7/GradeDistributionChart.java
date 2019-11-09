bf4c5ed79b0b33900cb66b30912ebf5afb7d0803
hnminh <48984218+hnminh@users.noreply.github.com>
2019-11-09 03:22:47 +0200

    minor modifications

--- assignments/src/assignments7/GradeDistributionChart.java
+++ assignments/src/assignments7/GradeDistributionChart.java
@@ -20,70 +20,63 @@ import javafx.scene.control.Button;
 
 public class GradeDistributionChart extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Grade Distribution");
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Grade Distribution");
+		Distribution values = new Distribution(0, 5);
+		final String[] names = {"0", "1", "2", "3", "4", "5"};
+
+		VBox all = new VBox();
         
-        Distribution values = new Distribution(0, 5);
-        final String[] names = {"0", "1", "2", "3", "4", "5"};
-        
-        VBox all = new VBox();
-        
-        // the input area
-        HBox input = new HBox();
-        Label inputLabel = new Label("Enter grade: ");
-        TextField gradeInput = new TextField();
-        Button insertGrade = new Button("Insert");
-        input.getChildren().add(inputLabel);
-        input.getChildren().add(gradeInput);
-        input.getChildren().add(insertGrade);
-        input.setAlignment(Pos.CENTER);
-        
-        
+		// the input area
+		HBox input = new HBox();
+		Label inputLabel = new Label("Enter grade: ");
+		TextField gradeInput = new TextField();
+		Button insertGrade = new Button("Insert");
+		input.getChildren().add(inputLabel);
+		input.getChildren().add(gradeInput);
+		input.getChildren().add(insertGrade);
+		input.setAlignment(Pos.CENTER);
+
+
         all.getChildren().add(input);
-        
-        // the barchart
-        final CategoryAxis xAxis = new CategoryAxis(); //String category
-        final NumberAxis yAxis = new NumberAxis();  
-        final BarChart<String, Number> barchart = new BarChart<>(xAxis,yAxis);
-        
-        barchart.setLegendVisible(false); //Just one series, legend is not needed
-        xAxis.setLabel("Grade");       
-        yAxis.setLabel("Frequency");
- 
+
+		// the barchart
+		final CategoryAxis xAxis = new CategoryAxis(); //String category
+		final NumberAxis yAxis = new NumberAxis();  
+		final BarChart<String, Number> barchart = new BarChart<>(xAxis,yAxis);
+
+		barchart.setLegendVisible(false); //Just one series, legend is not needed
+		xAxis.setLabel("Grade");       
+		yAxis.setLabel("Frequency");
+
         XYChart.Series<String, Number> distrib = new Series<>();
-        for(int i = 0; i < names.length; i++) {
-            distrib.getData().add(new Data<>(names[i], values.frequency(i)));
-        }
-        
-        insertGrade.setOnAction(new EventHandler<ActionEvent>(){
-        	@Override
-        	public void handle(ActionEvent e){
-        		if (!gradeInput.getText().equals("")) {
-                	int a = Integer.parseInt(gradeInput.getText());
-                	if (a >= 0 && a <= 5) {
-	                	values.insertValue(a);
-	                	distrib.getData().get(a).setYValue(values.frequency(a));
-                	}
-        		}
-        	}
-        });
-        barchart.getData().add(distrib);
-        all.getChildren().add(barchart);
-        
-        
-        /*Scene inputPart = new Scene(grid, 500, 100);
-        stage.setScene(inputPart);
-        stage.show();*/	
-        //grid.add(barchart, 1, 3);
-        Scene scene = new Scene(all,500,500);
-        
-        stage.setScene(scene);
-        stage.show();
-        	
-    }
-        
-    public static void main(String[] args) {
-    	launch(args);
-    }
+		for(int i = 0; i < names.length; i++) {
+			distrib.getData().add(new Data<>(names[i], values.frequency(i)));
+		}
+
+		insertGrade.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent e){
+				if (!gradeInput.getText().equals("")) {
+					int a = Integer.parseInt(gradeInput.getText());
+					if (a >= 0 && a <= 5) {
+						values.insertValue(a);
+						distrib.getData().get(a).setYValue(values.frequency(a));
+					}
+				}
+			}
+		});
+		barchart.getData().add(distrib);
+		all.getChildren().add(barchart);
+		
+		Scene scene = new Scene(all,500,500);
+		stage.setScene(scene);
+		stage.show();
+			
+	}
+	    
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
